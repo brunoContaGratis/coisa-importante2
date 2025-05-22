d13a6e4b4ad91bb77690fce33dfb3c0c6c75e6a4
brunoContaGratis <contagratisbruno123@gmail.com>
2025-05-21 22:41:33 -0300

    feat

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/coisa-importante2/", // Substitua pelo nome do seu repositório
+  base: "/coisa-importante2/", // <<< MUITO IMPORTANTE!
   plugins: [react()],
+  build: {
+    outDir: "build",
+  },
 });
